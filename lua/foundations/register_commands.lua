f44f93a4c63403ca18429b757756e39d99a5eb5c
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-25 07:11:07 -0700

    Added user commands
    User commands now exist for three main commands.
    No functionality is currently implemented.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/register_commands.lua
+++ lua/foundations/register_commands.lua
@@ -1,9 +1,43 @@
+local new_template = function()
+	print("new template")
+end
+
+local from_template = function()
+	print("from template")
+end
+
+local edit_template = function()
+	print("edit template")
+end
+local command_names = {
+	"NewTemplate",
+	"FromTemplate",
+	"EditTemplate",
+}
+local commands_functions = {
+	["NewTemplate"] = new_template,
+	["FromTemplate"] = from_template,
+	["EditTemplate"] = edit_template,
+}
+
 vim.api.nvim_create_user_command("Foundations", function(opts)
 	opts = opts or "~/.template"
+	print(vim.inspect(opts))
 	print(opts.args)
+	-- From here
+	local args = {}
+
+	for token in string.gmatch(opts.args, "[^%s]+") do
+		table.insert(args, token)
+	end
+	-- To here comes from this: https://stackoverflow.com/questions/1426954/split-string-in-lua
+	-- Stackoverflow question
+	for _, arg in pairs(args) do
+		commands_functions[arg]()
+	end
 end, {
 	nargs = 1,
 	complete = function(ArgLead, CmdLine, CursorPos)
-		return { "NewTemplate", "FromTemplate", "EditTemplate" }
+		return command_names
 	end,
 })
