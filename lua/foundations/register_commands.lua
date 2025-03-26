4d60d05f1aea29561cdd9c5b4df4fd5ee82290ad
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-26 08:21:52 -0700

    Improved user commands
    Currently EditTemplate and FromTemplate are completed as intended.
    NewTemplate needs to be modified as it currently operates the same as
    EditTemplate but should act as follows.
    Opens an entry buffer, auto complete on all directories in the templates
    folder as the user enters a path, on enter new template is created at
    path specified and is opened in new buffer.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/register_commands.lua
+++ lua/foundations/register_commands.lua
@@ -1,43 +1,55 @@
-local new_template = function()
-	print("new template")
-end
-
-local from_template = function()
-	print("from template")
-end
-
-local edit_template = function()
-	print("edit template")
-end
+local commands = require("foundations.commands")
 local command_names = {
-	"NewTemplate",
 	"FromTemplate",
+	"NewTemplate",
 	"EditTemplate",
 }
 local commands_functions = {
-	["NewTemplate"] = new_template,
-	["FromTemplate"] = from_template,
-	["EditTemplate"] = edit_template,
+	["NewTemplate"] = commands.new_template,
+	["FromTemplate"] = commands.from_template,
+	["EditTemplate"] = commands.edit_template,
 }
 
 vim.api.nvim_create_user_command("Foundations", function(opts)
 	opts = opts or "~/.template"
-	print(vim.inspect(opts))
-	print(opts.args)
-	-- From here
 	local args = {}
 
 	for token in string.gmatch(opts.args, "[^%s]+") do
 		table.insert(args, token)
 	end
-	-- To here comes from this: https://stackoverflow.com/questions/1426954/split-string-in-lua
-	-- Stackoverflow question
 	for _, arg in pairs(args) do
 		commands_functions[arg]()
 	end
 end, {
 	nargs = 1,
 	complete = function(ArgLead, CmdLine, CursorPos)
-		return command_names
+		local args = {}
+		for token in string.gmatch(CmdLine, "[^%s]+") do
+			table.insert(args, token)
+		end
+		-- args.1 is "Foundations", and will always be, just remove it
+		_ = table.remove(args, 1)
+		vim.api.nvim_err_writeln("LEN " .. vim.tbl_count(args) .. "")
+		vim.api.nvim_err_writeln("ARGLEAD " .. ArgLead)
+		vim.api.nvim_err_writeln("ARGLEAD size " .. string.len(ArgLead))
+		local args_count = vim.tbl_count(args)
+		if args_count >= 3 then
+			return {}
+		elseif args_count == 0 then
+			return command_names
+		elseif string.match(args[1], command_names[1]) then
+			return {}
+		elseif args_count == 2 or (args_count == 1 and string.len(ArgLead) == 0) then
+			return vim.tbl_filter(function(a)
+				vim.api.nvim_err_writeln(
+					vim.inspect(require("foundations.util").get_templates(require("foundations")._configs.path))
+				)
+				return string.match(a, ArgLead) ~= nil
+			end, require("foundations.util").get_templates(require("foundations")._configs.path))
+		else
+			return vim.tbl_filter(function(a)
+				return string.match(a, ArgLead) ~= nil
+			end, command_names)
+		end
 	end,
 })
