6a07175ba5cc362d963643609b0161556b1dd6e0
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-26 08:19:28 -0700

    Cleaned up init file
    Init files has had unnecessary code removed and the default config
    refined a bit.
    Default config will likely be moved to a different file at some point.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/init.lua
+++ lua/foundations/init.lua
@@ -4,19 +4,33 @@ local M = {}
 --	A list of all the available templates
 --	Need to be of the form of template_spec
 --]]
-M._template = {}
+M._templates = {}
 M._configs = {
 	path = "~/.config/nvim/templates",
+	replacements = {
+		title = {
+			from = "{{__title__}}",
+			to = function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		},
+		time = {
+			from = "{{__time__}}",
+			to = function()
+				return vim.uv.clock_gettime()
+			end,
+		},
+	},
 }
-M.setup = function(config)
+M.config = function(config)
 	if config ~= nil and config.path ~= nil then
-		M._configs.path = config.path
+		M._configs = vim.tbl_deep_extend("force", M._configs, config)
 	end
-
+end
+M.setup = function(config)
+	M.config(config)
 	require("foundations.register_commands")
+	M._templates = require("foundations.util").get_templates(M._configs.path)
 end
 
-M.new_template = function(template_spec) end
-
-M.file_from_template = function(template, name) end
 return M
