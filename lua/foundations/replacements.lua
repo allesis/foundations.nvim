9a30adc96c5e738d5c4c99cd49dd87186e55fc6b
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 12:18:51 -0700

    Changed replacements to align with function signature
    Replacements take a single argument which is the text they were matched
    on.
    Usually is just from option but may be different if wildcards are used
    in matching and can then be used in replacement function.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/replacements.lua
+++ lua/foundations/replacements.lua
@@ -4,32 +4,35 @@ local M = {}
 -- Commonly used
 M.date = {
 	from = "{{__date__}}",
-	to = function()
-		return vim.api.nvim_exec2("!date", { output = true }).output
+	to = function(match)
+		local res = vim.api.nvim_exec2("!date", { output = true }).output
+		return res
 	end,
 }
 
 M.title = {
 	from = "{{__title__}}",
-	to = function()
+	to = function(match)
 		return vim.fs.basename(vim.api.nvim_buf_get_name(0))
 	end,
+	post_create = true,
 }
 M.time = {
 	from = "{{__time__}}",
-	to = function()
-		return vim.uv.clock_gettime()
+	to = function(match)
+		return vim.uv.clock_gettime("realtime").sec
 	end,
 }
 M.path = {
 	from = "{{__path__}}",
-	to = function()
-		return vim.api.nvim_buf_get_name(0)
+	to = function(match)
+		local path = vim.api.nvim_buf_get_name(0)
 	end,
+	post_create = true,
 }
 M.name = {
 	from = "{{__name__}}",
-	to = function()
+	to = function(match)
 		return vim.uv.os_get_passwd().username
 	end,
 }
@@ -41,27 +44,34 @@ M.name = {
 -- Really unlikely to be used,
 -- but you never know
 
+--[[
 -- These two are here entirely to allow entering the template strings directly into a template without replacement
 -- So {{__left__}}some_template_string{{__right__}}
 -- will become {{__some_template_string__}} after replacements have been made
 M.left = {
 	from = "{{__left__}}",
-	to = "{{__",
+	to = function(match)
+		return "{{__"
+	end,
 }
 
 M.right = {
 	from = "{{__right__}}",
-	to = "__}}",
+	to = function(match)
+		return "__}}"
+	end,
 }
-
+--]]
 -- Wrappers around command line functions
 -- These are just here to allow dedicated users to go to town on templates
 
 M.date_with_string = {
-	from = "({{__date__(.*)__}})",
-	to = function(from)
-		from = from or ""
-		return vim.cmd('!date +""')
+	from = "{{__date__(.*)__}}",
+	to = function(match)
+		match = match or ""
+		match = string.gsub(match, "{{__date__", "")
+		match = string.gsub(match, "__}}", "")
+		return vim.api.nvim_exec2('!date +"' .. match .. '"', { output = true }).output
 	end,
 }
 return M
