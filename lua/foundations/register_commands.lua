9c119e89ed120fbd74cc1106989f0b1a6717894c
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 06:50:27 -0700

    Changed autocomplete command behavior
    Previous impl was a holdover from command line entry of file and
    template path. Changed command autocomplete to instead simply match the
    command the throw to telescope as is intended.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/register_commands.lua
+++ lua/foundations/register_commands.lua
@@ -21,23 +21,9 @@ vim.api.nvim_create_user_command("Foundations", function(opts)
 end, {
 	nargs = 1,
 	complete = function(ArgLead, CmdLine, CursorPos)
-		local args = {}
-		for token in string.gmatch(CmdLine, "[^%s]+") do
-			table.insert(args, token)
-		end
-		-- args.1 is "Foundations", and will always be, just remove it
-		_ = table.remove(args, 1)
-		local args_count = vim.tbl_count(args)
-		if args_count >= 3 then
+		local _, args_count = string.gsub(CmdLine, " ", " ")
+		if args_count > 1 then
 			return {}
-		elseif args_count == 0 then
-			return command_names
-		elseif string.match(args[1], command_names[1]) then
-			return {}
-		elseif args_count == 2 or (args_count == 1 and string.len(ArgLead) == 0) then
-			return vim.tbl_filter(function(a)
-				return string.match(a, ArgLead) ~= nil
-			end, require("foundations.util").get_templates(require("foundations")._configs.path))
 		else
 			return vim.tbl_filter(function(a)
 				return string.match(a, ArgLead) ~= nil
