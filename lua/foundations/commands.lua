9c4774cc01b22cc75309609598e8fe1c7d2c4d29
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 05:51:35 -0700

    Added from_template
    Prompts the user to select a template the same way edit_template does.
    Then gets a path and file name from the user and calls
    util.file_from_template with the choosen template and file path as args.
    Currently does not get the correct root dir for selecting the file path.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/commands.lua
+++ lua/foundations/commands.lua
@@ -93,6 +93,49 @@ end
 
 M.from_template = function(opts)
 	opts = opts or {}
+	local finder = finders.new_table({
+		results = util.get_templates(),
+		entry_maker = function(entry)
+			return {
+				value = entry,
+				display = entry,
+				ordinal = vim.fs.normalize(entry),
+			}
+		end,
+	})
+	pickers
+		.new(opts, {
+			prompt_title = "New File From Template",
+			finder = finder,
+			sorter = conf.file_sorter(opts),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					-- We are reusing the code in new_template to get us a valid file path
+					-- 	without immedietly switching to it and closing it on save
+					-- We do this by overloading the callback function it calls
+					-- 	and spoofing some options
+					-- The variable names are a bit confusing since the called function
+					-- 	is not doing what the variable names would imply
+					-- Here opts.from_template_path is the actual full template path
+					-- In the callback, template_path is the file_path
+					-- 	and file_namthe e is the file name
+					-- HACK: This is a bad way to do things, either rename things
+					-- 		to make it less confusing, or make the code less confusing
+					local template_path = action_state.get_selected_entry().value
+					opts.from_template_path = template_path
+					opts.title = "New File Name"
+					opts.prompt_title = "New File Path"
+					opts.path = util.get_project_root(vim.api.nvim_buf_get_name(0))
+					opts.callback = function(file_name, o)
+						util.file_from_template(o.from_template_path, o.template_path .. "/" .. file_name)
+					end
+					M.new_template(opts)
+				end)
+				return true
+			end,
+		})
+		:find()
 end
-end
+
 return M
