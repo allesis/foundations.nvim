843881babedb8a29d798ddf6483be67e8e515e17
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 05:03:44 -0700

    Added edit_template
    Takes a template path and opens it for editing.
    Closes the path upon saving buffer or file and returns to the previous
    buffer. Returns to the intro screen if no buffer exists to return to.
    Intro screen can be changed to whatever functionaliy is desired by
    modifying the `intro` config variable.
    Default value is vim.intro or :Alpha depending on if alpha.nvim is
    installed. Will all options for startify and snacks.nvim at some point.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/commands.lua
+++ lua/foundations/commands.lua
@@ -5,6 +5,25 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local util = require("foundations.util")
 local M = {}
+local edit_template = function(template_path)
+	local oldbuf = vim.api.nvim_get_current_buf()
+	--TODO: This will break on specific paths, e.g. leading ~ or /
+	template_path = vim.fs.normalize(template_path)
+	vim.cmd.e(template_path)
+	local newbuf = vim.api.nvim_get_current_buf()
+	vim.api.nvim_create_autocmd({ "BufWritePost", "FileWritePost" }, {
+		buffer = newbuf,
+		desc = "Return to previous buffer after exiting a template edit buffer.",
+		callback = function()
+			if vim.api.nvim_buf_is_valid(oldbuf) then
+				vim.api.nvim_set_current_buf(oldbuf)
+			else
+				require("foundations")._configs.intro()
+			end
+			vim.api.nvim_buf_delete(newbuf, { force = true })
+		end,
+	})
+end
 M.new_template = function(opts)
 	opts = opts or {}
 M.edit_template = function(opts)
