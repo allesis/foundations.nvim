d724f42097c8fc1c30d33fcb6d02f14f429b6b2f
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 05:00:55 -0700

    Added command functions
    Added command functions and needed imports for telescope integration
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

---
+++ lua/foundations/commands.lua
@@ -0,0 +1,17 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local util = require("foundations.util")
+local M = {}
+M.new_template = function(opts)
+	opts = opts or {}
+M.edit_template = function(opts)
+	opts = opts or {}
+end
+M.from_template = function(opts)
+	opts = opts or {}
+end
+end
+return M
