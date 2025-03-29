4e360e7afa3c24f350789ce5a22d6a6db7c9f862
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-29 11:04:11 -0700

    Updated replacements
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/replacements.lua
+++ lua/foundations/replacements.lua
@@ -1,19 +1,67 @@
 local M = {}
 
+-- Basic Replacements
+-- Commonly used
+M.date = {
+	from = "{{__date__}}",
+	to = function()
+		return vim.api.nvim_exec2("!date", { output = true }).output
+	end,
+}
+
 M.title = {
 	from = "{{__title__}}",
 	to = function()
-		return vim.api.nvim_buf_get_name(0)
+		return vim.fs.basename(vim.api.nvim_buf_get_name(0))
 	end,
 }
-M.time = {
-	from = "{{__time__}}",
+M.fulldate = {
+	from = "{{__fulldate__}}",
 	to = function()
-		return vim.uv.clock_gettime()
+		return vim.cmd('!date +"%A the %d  of %B, %Y"')
 	end,
 }
 M.path = {
 	from = "{{__path__}}",
-	to = function() end,
+	to = function()
+		return vim.api.nvim_buf_get_name(0)
+	end,
+}
+M.name = {
+	from = "{{__name__}}",
+	to = function()
+		return vim.uv.os_get_passwd().username
+	end,
+}
+
+-- Advanced replacements
+-- These use lua magic and do useful things
+
+-- Niece replacements
+-- Really unlikely to be used,
+-- but you never know
+
+-- These two are here entirely to allow entering the template strings directly into a template without replacement
+-- So {{__left__}}some_template_string{{__right__}}
+-- will become {{__some_template_string__}} after replacements have been made
+M.left = {
+	from = "{{__left__}}",
+	to = "{{__",
+}
+
+M.right = {
+	from = "{{__right__}}",
+	to = "__}}",
+}
+
+-- Wrappers around command line functions
+-- These are just here to allow dedicated users to go to town on templates
+
+M.date_with_string = {
+	from = "({{__date__(.*)__}})",
+	to = function(from)
+		from = from or ""
+		return vim.cmd('!date +""')
+	end,
 }
 return M
