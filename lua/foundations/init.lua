8de1d51aeb565dd7be801108a2d1ab97e1cc3acf
Sean Allesina-McGrory <allesis@wwu.edu>
2025-03-30 13:52:01 -0700

    Removed configs.templates
    No longer use a template spec.
    May be introduced again later when project templates are added (if) but
    for now removing due to dead code.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

--- lua/foundations/init.lua
+++ lua/foundations/init.lua
@@ -4,7 +4,6 @@ local M = {}
 --	A list of all the available templates
 --	Need to be of the form of template_spec
 --]]
-M._templates = {}
 local replacements = require("foundations.replacements")
 M._configs = {
 	path = "~/.config/nvim/templates",
@@ -28,7 +27,6 @@ end
 M.setup = function(config)
 	M.config(config)
 	require("foundations.register_commands")
-	M._templates = require("foundations.util").get_templates(M._configs.path)
 end
 
 return M
